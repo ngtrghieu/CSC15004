163f4e28eb1a3e3d07ce49743ef1bc550e8d738b
ngtrghieu <20120083@student.hcmus.edu.vn>
2023-06-19 08:50:15 +0700

    generate reviewText

--- CSC15004_project/final_lab/urls.py
+++ CSC15004_project/final_lab/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("clone_index", views.Clone_indexView.as_view(), name="clone_index"),
     path("index", views.IndexView.as_view(), name="index"),
     path("base", views.BaseView.as_view(), name="base"),
+    path('index/<str:category>/', views.index_view, name='category'),
+
+    # path("cat", views.cat_view, name="cat"),
 ]
--- CSC15004_project/final_lab/views.py
+++ CSC15004_project/final_lab/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from django.views import generic
+from django.views import generic, View
 from django.utils import timezone
 
 from .models import AmazonLabels, AmazonProductReviews
@@ -18,3 +19,25 @@ class IndexView(generic.TemplateView):
 
 class BaseView(generic.TemplateView):
     template_name = "final_lab/base.html"
+
+# def cat_view(request):
+#     return render(request, 'final_lab/cat.html')
+
+# def index_view(request, category):
+#     label = ?
+#     return render(request, 'final_lab/category.html', {'category': category, 'label': label})
+
+def index_view(request, category):
+    try:
+        label = AmazonLabels.objects.get(cat_name=category)
+    except AmazonLabels.DoesNotExist:
+        label = None
+        rnd_txt = None
+    else:
+        reviews_with_label = AmazonProductReviews.objects.filter(label=label)
+        if reviews_with_label.exists():
+            rnd_txt = random.choice(reviews_with_label).reviewText
+        else:
+            rnd_txt = None
+
+    return render(request, 'final_lab/category.html', {'category': category, 'label_cat': label, 'reviewText': rnd_txt})
